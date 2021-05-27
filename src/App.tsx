eeed5cf829254e3703f2bee8eecdd53ee08f80cc
madeinchema <chemavg1@gmail.com>
2021-05-27 23:41:10 +0200

    add Header layout comp + styles + inside App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/layout/Header';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Header />
     </div>
   );
 }
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,19 @@
+import './header.scss'
+
+const Header = () => {
+  return (
+    <div className='header'>
+
+      <div className="logo">
+        <img  className="logo--img" src="./logo.png" alt="" />
+      </div>
+      <nav className='nav'>
+        <li className="nav--item">Clientes</li>
+        <li className="nav--item">Productos</li>
+        <li className="nav--item">Cerrar sesión</li>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
