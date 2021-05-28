26a87d8ed1836e4f854063890ced1b9c4b943379
madeinchema <chemavg1@gmail.com>
2021-05-28 10:10:40 +0200

    add ButtonGroup common comp & export it

---
+++ src/components/common/ButtonGroup/index.tsx
@@ -0,0 +1,34 @@
+import { Fragment } from 'react'
+import Button, { ButtonProps } from '../Button'
+import './styles.scss'
+
+type ButtonGroupDataSource = ButtonProps & {
+  id: string
+}
+
+type ButtonGroupProps = {
+  dataSource: ButtonGroupDataSource[]
+}
+
+const ButtonGroup = ({ dataSource }: ButtonGroupProps): JSX.Element => {
+  return (
+    <div className="button-group">
+      {dataSource.map((button, index) => {
+        return (
+          <Fragment key={button.id}>
+            <Button
+              onClick={button.onClick}
+              variant={button.variant}
+              className={button.className}
+            >
+              {button.children}
+            </Button>
+            {index % 2 === 0 && <div className="separator" />}
+          </Fragment>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ButtonGroup
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './layout/Header'
 
 export { default as Button } from './common/Button'
+export { default as ButtonGroup } from './common/ButtonGroup'
