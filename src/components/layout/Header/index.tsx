933ef78d847388d6b9573a6481fc77607467381b
madeinchema <chemavg1@gmail.com>
2021-05-28 10:01:11 +0200

    add index.ts file to export components

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,13 +1,12 @@
-import './header.scss'
+import './styles.scss'
 
-const Header = () => {
+const Header = (): JSX.Element => {
   return (
-    <div className='header'>
-
+    <div className="header">
       <div className="logo">
-        <img  className="logo--img" src="./logo.png" alt="" />
+        <img className="logo--img" src="./logo.png" alt="" />
       </div>
-      <nav className='nav'>
+      <nav className="nav">
         <li className="nav--item">Clientes</li>
         <li className="nav--item">Productos</li>
         <li className="nav--item">Cerrar sesión</li>
