5d5734da078ab7030f2a0eff5296e03c31808aff
madeinchema <chemavg1@gmail.com>
2021-05-29 12:20:34 +0200

    add server's db.json & generateClients.js files

---
+++ server/generateClients.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+const fs = require('fs');
+const { name, image, internet } = require('faker');
+
+
+const database = { clients: [] }
+
+for (let i = 1; i <= 20; i++) {
+  const newUser = {
+    id: i,
+    name: name.firstName(),
+    email: internet.email(),
+    password: internet.password(),
+    avatar: image.avatar(),
+  }
+
+  database.clients.push(newUser)
+}
+
+fs.writeFile('./server/db.json', JSON.stringify(database), function (err) {
+  if (err) return console.log(err)
+  console.log('Generated Users Database > ./server/db.json')
+})
