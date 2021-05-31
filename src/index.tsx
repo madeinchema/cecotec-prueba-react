a8125cf5d19559442b5068ce4b0d9ee72ec223c4
madeinchema <chemavg1@gmail.com>
2021-05-31 07:41:57 +0200

    add Apollo client provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import store from './state/store'
 
 import App from './App'
 import './index.scss'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
+  </StrictMode>,
   document.getElementById('root')
 )
