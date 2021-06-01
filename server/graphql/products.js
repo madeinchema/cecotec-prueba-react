16f21befaee8dc8189d1f49c15cbc160da233814
madeinchema <chemavg1@gmail.com>
2021-06-01 14:22:03 +0200

    update products mock data with real images

--- server/graphql/products.js
+++ server/graphql/products.js
@@ -1,80 +1,93 @@
 module.exports = [
   {
-    "id": "MTkyOTA5NzI1NA==",
-    "name": "Unbranded Concrete Table",
-    "price": "17.00",
-    "image": "https://source.unsplash.com/random/?product,thing#44341"
+    id: 'MTkyOTA5NzI1NA==',
+    name: 'Unbranded Concrete Table',
+    price: '17.00',
+    image:
+      'https://images.unsplash.com/photo-1615615228002-890bb61cac6e?&w=600',
   },
   {
-    "id": "Mjg1MDU3OTU0MA==",
-    "name": "Practical Soft Soap",
-    "price": "151.00",
-    "image": "https://source.unsplash.com/random/?product,thing#7997"
+    id: 'Mjg1MDU3OTU0MA==',
+    name: 'Practical Soft Soap',
+    price: '151.00',
+    image:
+      'https://images.unsplash.com/photo-1486401899868-0e435ed85128?crop=entropy&w=600',
   },
   {
-    "id": "ODc4MDcxODgy",
-    "name": "Unbranded Plastic Pants",
-    "price": "20.00",
-    "image": "https://source.unsplash.com/random/?product,thing#68133"
+    id: 'ODc4MDcxODgy',
+    name: 'Unbranded Plastic Pants',
+    price: '20.00',
+    image:
+      'https://images.unsplash.com/photo-1615396899839-c99c121888b0?crop=entropy&w=600',
   },
   {
-    "id": "MTM5MTE5OTQwMw==",
-    "name": "Sleek Rubber Towels",
-    "price": "102.00",
-    "image": "https://source.unsplash.com/random/?product,thing#60293"
+    id: 'MTM5MTE5OTQwMw==',
+    name: 'Sleek Rubber Towels',
+    price: '102.00',
+    image:
+      'https://images.unsplash.com/photo-1493723843671-1d655e66ac1c?crop=entropy&w=600',
   },
   {
-    "id": "Nzk3NTYyNTM3MA==",
-    "name": "Incredible Soft Sausages",
-    "price": "10.00",
-    "image": "https://source.unsplash.com/random/?product,thing#34798"
+    id: 'Nzk3NTYyNTM3MA==',
+    name: 'Incredible Soft Sausages',
+    price: '10.00',
+    image:
+      'https://images.unsplash.com/photo-1616401776146-ae3453da7105?crop=entropy&w=600',
   },
   {
-    "id": "ODYwNzU2NTU1Mw==",
-    "name": "Ergonomic Rubber Shirt",
-    "price": "91.00",
-    "image": "https://source.unsplash.com/random/?product,thing#67704"
+    id: 'ODYwNzU2NTU1Mw==',
+    name: 'Ergonomic Rubber Shirt',
+    price: '91.00',
+    image:
+      'https://images.unsplash.com/photo-1541643600914-78b084683601?crop=entropy&w=600',
   },
   {
-    "id": "MTY5MzUxOTIw",
-    "name": "Gorgeous Soft Chips",
-    "price": "173.00",
-    "image": "https://source.unsplash.com/random/?product,thing#13284"
+    id: 'MTY5MzUxOTIw',
+    name: 'Gorgeous Soft Chips',
+    price: '173.00',
+    image:
+      'https://images.unsplash.com/photo-1506152983158-b4a74a01c721?crop=entropy&w=600',
   },
   {
-    "id": "MTY4MTc0MDMzMA==",
-    "name": "Refined Plastic Chair",
-    "price": "170.00",
-    "image": "https://source.unsplash.com/random/?product,thing#69609"
+    id: 'MTY4MTc0MDMzMA==',
+    name: 'Refined Plastic Chair',
+    price: '170.00',
+    image:
+      'https://images.unsplash.com/photo-1454944338482-a69bb95894af?crop=entropy&w=600',
   },
   {
-    "id": "MTIyNTc0MDc2MA==",
-    "name": "Ergonomic Soft Hat",
-    "price": "16.00",
-    "image": "https://source.unsplash.com/random/?product,thing#829"
+    id: 'MTIyNTc0MDc2MA==',
+    name: 'Ergonomic Soft Hat',
+    price: '16.00',
+    image:
+      'https://images.unsplash.com/photo-1559327875-12005444b626?crop=entropy&w=600',
   },
   {
-    "id": "OTc3ODQ5NDc0OA==",
-    "name": "Small Plastic Pizza",
-    "price": "171.00",
-    "image": "https://source.unsplash.com/random/?product,thing#61452"
+    id: 'OTc3ODQ5NDc0OA==',
+    name: 'Small Plastic Pizza',
+    price: '171.00',
+    image:
+      'https://images.unsplash.com/photo-1511235873174-1888843f2ab0?crop=entropy&w=600',
   },
   {
-    "id": "ODc2OTU0NDg0",
-    "name": "Sleek Soft Chips",
-    "price": "97.00",
-    "image": "https://source.unsplash.com/random/?product,thing#2833"
+    id: 'ODc2OTU0NDg0',
+    name: 'Sleek Soft Chips',
+    price: '97.00',
+    image:
+      'https://images.unsplash.com/photo-1505508520564-7e2642683d77?crop=entropy&w=600',
   },
   {
-    "id": "NjcwNzkwODg3NA==",
-    "name": "Handcrafted Plastic Chicken",
-    "price": "172.00",
-    "image": "https://source.unsplash.com/random/?product,thing#80319"
+    id: 'NjcwNzkwODg3NA==',
+    name: 'Handcrafted Plastic Chicken',
+    price: '172.00',
+    image:
+      'https://images.unsplash.com/photo-1563891217861-7924b471afb3?crop=entropy&w=600',
   },
   {
-    "id": "MzYwMTM4NjkwNw==",
-    "name": "Gorgeous Granite Shirt",
-    "price": "55.00",
-    "image": "https://source.unsplash.com/random/?product,thing#69104"
-  }
+    id: 'MzYwMTM4NjkwNw==',
+    name: 'Gorgeous Granite Shirt',
+    price: '55.00',
+    image:
+      'https://images.unsplash.com/photo-1585387047269-e66bf53002f5?crop=entropy&w=600',
+  },
 ]
