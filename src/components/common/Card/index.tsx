299362a19f3e3d18ff8bde36f8d7e3f8f05ea67f
madeinchema <chemavg1@gmail.com>
2021-06-01 21:50:46 +0200

    add Card common comp

---
+++ src/components/common/Card/index.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react'
+import ButtonGroup from '../ButtonGroup'
+import './styles.scss'
+
+interface CardProps {
+  children: ReactNode
+  className?: string
+  buttonGroupDataSource: {
+    id: string
+    children: string
+    onClick: () => void
+  }[]
+}
+
+const Card = ({
+  children,
+  className,
+  buttonGroupDataSource,
+}: CardProps): JSX.Element => {
+  const propsClassName = `${className}-card` || ''
+
+  return (
+    <div className={`${propsClassName} card`}>
+      <div className={`${propsClassName} card--container`}>{children}</div>
+      <ButtonGroup dataSource={buttonGroupDataSource} />
+    </div>
+  )
+}
+
+export default Card
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as Header } from './layout/Header'
 export { default as Layout } from './layout/Layout'
 export { default as Portal } from './layout/Portal'
 
+export { default as Card } from './common/Card'
 export { default as Button } from './common/Button'
 export { default as ButtonGroup } from './common/ButtonGroup'
 export { default as ModalConfirm } from './common/ModalConfirm'
