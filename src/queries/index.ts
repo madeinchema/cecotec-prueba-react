41e08bd0545af8b4b62738ad1c0bb128c8591472
madeinchema <chemavg1@gmail.com>
2021-06-01 13:16:29 +0200

    add graphql queries file

---
+++ src/queries/index.ts
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client'
+
+/**
+ * Queries
+ */
+export const GET_ALL_PRODUCTS = gql`
+  query GetAllProducts {
+    allProducts {
+      id
+      name
+      price
+      image
+    }
+  }
+`
+
+export const GET_PRODUCT = gql`
+  query GetProduct($id: ID!) {
+    product(id: $id) {
+      id
+      name
+      price
+      image
+    }
+  }
+`
+
+/**
+ * Mutations
+ */
+export const ADD_PRODUCT = gql`
+  mutation AddProduct($id: ID!, $name: String!, $price: String!) {
+    addProduct(name: $name, price: $price) {
+      id
+      name
+      price
+      image
+    }
+  }
+`
+
+export const REMOVE_PRODUCT = gql`
+  mutation RemoveProduct($id: ID!) {
+    removeProduct(id: $id) {
+      id
+      name
+      price
+      image
+    }
+  }
+`
+
+export const EDIT_PRODUCT = gql`
+  mutation EditProduct($id: ID!, $name: String!, $price: String!) {
+    editProduct(id: $id, name: $name, price: $price) {
+      id
+      name
+      price
+    }
+  }
+`
