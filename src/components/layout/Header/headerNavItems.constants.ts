024ac01ab7f2da5dc3e62ad2068b370e53b537b8
madeinchema <chemavg1@gmail.com>
2021-06-02 21:53:31 +0200

    add constants for Header comp's headerNavItems

---
+++ src/components/layout/Header/headerNavItems.constants.ts
@@ -0,0 +1,24 @@
+const headerNavItems = {
+  loggedIn: [
+    {
+      label: 'Clientes',
+      link: '/clients',
+    },
+    {
+      label: 'Productos',
+      link: '/products',
+    },
+    {
+      label: 'Cerrar sesión',
+      link: '/',
+    },
+  ],
+  loggedOut: [
+    {
+      label: 'Iniciar sesión',
+      link: '/',
+    },
+  ],
+}
+
+export { headerNavItems }
