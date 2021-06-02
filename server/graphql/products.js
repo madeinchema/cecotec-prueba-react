539d55821ab19f6773adbf50dede0932e9b193e7
madeinchema <chemavg1@gmail.com>
2021-06-02 11:53:40 +0200

    update graphql products' price format

--- server/graphql/products.js
+++ server/graphql/products.js
@@ -2,91 +2,91 @@ module.exports = [
   {
     id: 'MTkyOTA5NzI1NA==',
     name: 'Unbranded Concrete Table',
-    price: '17.00',
+    price: '17,00',
     image:
       'https://images.unsplash.com/photo-1615615228002-890bb61cac6e?&w=600',
   },
   {
     id: 'Mjg1MDU3OTU0MA==',
     name: 'Practical Soft Soap',
-    price: '151.00',
+    price: '151,00',
     image:
       'https://images.unsplash.com/photo-1486401899868-0e435ed85128?crop=entropy&w=600',
   },
   {
     id: 'ODc4MDcxODgy',
     name: 'Unbranded Plastic Pants',
-    price: '20.00',
+    price: '20,00',
     image:
       'https://images.unsplash.com/photo-1615396899839-c99c121888b0?crop=entropy&w=600',
   },
   {
     id: 'MTM5MTE5OTQwMw==',
     name: 'Sleek Rubber Towels',
-    price: '102.00',
+    price: '102,00',
     image:
       'https://images.unsplash.com/photo-1493723843671-1d655e66ac1c?crop=entropy&w=600',
   },
   {
     id: 'Nzk3NTYyNTM3MA==',
     name: 'Incredible Soft Sausages',
-    price: '10.00',
+    price: '10,00',
     image:
       'https://images.unsplash.com/photo-1616401776146-ae3453da7105?crop=entropy&w=600',
   },
   {
     id: 'ODYwNzU2NTU1Mw==',
     name: 'Ergonomic Rubber Shirt',
-    price: '91.00',
+    price: '91,00',
     image:
       'https://images.unsplash.com/photo-1541643600914-78b084683601?crop=entropy&w=600',
   },
   {
     id: 'MTY5MzUxOTIw',
     name: 'Gorgeous Soft Chips',
-    price: '173.00',
+    price: '173,00',
     image:
       'https://images.unsplash.com/photo-1506152983158-b4a74a01c721?crop=entropy&w=600',
   },
   {
     id: 'MTY4MTc0MDMzMA==',
     name: 'Refined Plastic Chair',
-    price: '170.00',
+    price: '170,00',
     image:
       'https://images.unsplash.com/photo-1454944338482-a69bb95894af?crop=entropy&w=600',
   },
   {
     id: 'MTIyNTc0MDc2MA==',
     name: 'Ergonomic Soft Hat',
-    price: '16.00',
+    price: '16,00',
     image:
       'https://images.unsplash.com/photo-1559327875-12005444b626?crop=entropy&w=600',
   },
   {
     id: 'OTc3ODQ5NDc0OA==',
     name: 'Small Plastic Pizza',
-    price: '171.00',
+    price: '171,00',
     image:
       'https://images.unsplash.com/photo-1511235873174-1888843f2ab0?crop=entropy&w=600',
   },
   {
     id: 'ODc2OTU0NDg0',
     name: 'Sleek Soft Chips',
-    price: '97.00',
+    price: '97,00',
     image:
       'https://images.unsplash.com/photo-1505508520564-7e2642683d77?crop=entropy&w=600',
   },
   {
     id: 'NjcwNzkwODg3NA==',
     name: 'Handcrafted Plastic Chicken',
-    price: '172.00',
+    price: '172,00',
     image:
       'https://images.unsplash.com/photo-1563891217861-7924b471afb3?crop=entropy&w=600',
   },
   {
     id: 'MzYwMTM4NjkwNw==',
     name: 'Gorgeous Granite Shirt',
-    price: '55.00',
+    price: '55,00',
     image:
       'https://images.unsplash.com/photo-1585387047269-e66bf53002f5?crop=entropy&w=600',
   },
