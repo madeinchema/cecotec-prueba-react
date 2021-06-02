78fed0a1ca05a00a62157e8fa1d07aa625d64a73
madeinchema <chemavg1@gmail.com>
2021-06-02 22:00:03 +0200

    add & handle authentication in Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,26 +1,95 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router'
 import { Button } from '../../components'
+import { loadCurrentUser } from '../../state/slices/currentUserSlice'
 import './login.scss'
 
 const Login = (): JSX.Element => {
+  const SESSION_KEY = 'session'
+  const [formData, setFormData] = useState<{ [key: string]: string }>({
+    email: '',
+    password: '',
+  })
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  const handleFormOnChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target
+    setFormData(prevState => ({ ...prevState, [name]: value }))
+  }
+
+  function loadUserData(): void {
+    const sessionData = localStorage.getItem(SESSION_KEY) || ''
+    fetch(`${process.env.REACT_APP_SERVER_URL}/user`, {
+      headers: {
+        token: sessionData,
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        localStorage.setItem('user', JSON.stringify(data))
+        dispatch(loadCurrentUser())
+        history.push('/clients')
+      })
+  }
+
+  const handleLoginResponse = (): void => {
+    fetch(`${process.env.REACT_APP_SERVER_URL}/login_get`)
+      .then(res => res.json())
+      .then(data => {
+        localStorage.setItem(SESSION_KEY, JSON.stringify(data))
+        loadUserData()
+      })
+  }
+
+  const handleLoginSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    fetch(`${process.env.REACT_APP_SERVER_URL}/login_post`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(res => res.json())
+      .then(() => {
+        handleLoginResponse()
+      })
+  }
+
   return (
     <div className="login">
       <div className="login--container">
         <div className="login--logo">
           <img src="./logo.png" alt="" />
         </div>
-        <form action="">
+        <form onSubmit={handleLoginSubmit}>
           <label htmlFor="email">
             Correo electrónico
-            <input id="email" type="email" />
+            <input
+              name="email"
+              id="email"
+              type="email"
+              value={formData.email}
+              onChange={handleFormOnChange}
+            />
           </label>
           <label htmlFor="password">
             Contraseña
-            <input id="password" type="password" />
+            <input
+              name="password"
+              id="password"
+              type="password"
+              value={formData.password}
+              onChange={handleFormOnChange}
+            />
           </label>
+          <Button type="submit" className="login--button" variant="primary">
+            Iniciar sesión
+          </Button>
         </form>
-        <Button className="login--button" variant="primary">
-          Iniciar sesión
-        </Button>
         <p className="login--alternative-action">¿No tienes cuenta?</p>
       </div>
     </div>
