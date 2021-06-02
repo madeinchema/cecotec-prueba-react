a62b9335d1a629c3b37693013cb339aed74bb78d
madeinchema <chemavg1@gmail.com>
2021-06-03 00:10:08 +0200

    render avatar in Header & improve its styles

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -5,21 +5,30 @@ import { headerNavItems } from './headerNavItems.constants'
 
 const Header = (): JSX.Element => {
   const currentUserSelector = useTypedSelector(state => state.currentUser)
-
-  const isLoggedIn = currentUserSelector.data ? 'loggedIn' : 'loggedOut'
+  const isLoggedIn = !!currentUserSelector.data
+  const isLoggedInKey = currentUserSelector.data ? 'loggedIn' : 'loggedOut'
 
   return (
     <div className="header">
-      <div className="header--container">
-        <Link className="logo--img" to="/">
+      <div className="header__container">
+        <Link className="header__logo" to="/">
           <img src="./logo.png" alt="" />
         </Link>
-        <nav className="nav">
-          {headerNavItems[isLoggedIn].map(navItem => (
-            <li key={navItem.label} className="nav--item">
+
+        <nav className="header__nav">
+          {headerNavItems[isLoggedInKey].map(navItem => (
+            <li key={navItem.label} className="header__nav-item">
               <Link to={navItem.link}>{navItem.label}</Link>
             </li>
           ))}
+          {isLoggedIn && (
+            <div className="header__nav-item--avatar">
+              <img
+                src={currentUserSelector.data.avatar}
+                alt="username avatar"
+              />
+            </div>
+          )}
         </nav>
       </div>
     </div>
