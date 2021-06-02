65d41b6718e277834efc95916c3705a7e97ba5d1
madeinchema <chemavg1@gmail.com>
2021-06-02 21:00:39 +0200

    add currentUserSlice

---
+++ src/state/slices/currentUserSlice.ts
@@ -0,0 +1,83 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+/**
+ * Types
+ */
+interface CurrentUserData {
+  id: number | undefined
+  firstName: string | undefined
+  lastName: string | undefined
+  email: string | undefined
+  avatar: string | undefined
+}
+
+/**
+ * State
+ */
+export type ClientsSliceState = {
+  data: CurrentUserData
+  loading: boolean
+  success: boolean | undefined
+  error: string | undefined
+}
+
+const currentUserInitialState = {
+  id: undefined,
+  firstName: undefined,
+  lastName: undefined,
+  email: undefined,
+  avatar: undefined,
+}
+
+const initialState: ClientsSliceState = {
+  data: currentUserInitialState,
+  loading: false,
+  success: false,
+  error: undefined,
+}
+
+/**
+ * Thunks
+ */
+const loadCurrentUser = createAsyncThunk(
+  'currentUser/loadCurrentUser',
+  async () => {
+    const USER_KEY = 'user'
+    const currentUser: CurrentUserData = JSON.parse(
+      localStorage.getItem(USER_KEY) || '{}'
+    ) as CurrentUserData
+    return currentUser
+  }
+)
+
+/**
+ * Slice
+ */
+export const currentUserSlice = createSlice({
+  name: 'currentUser',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(loadCurrentUser.pending, state => {
+      state.loading = true
+      state.success = undefined
+      state.error = undefined
+    })
+    builder.addCase(loadCurrentUser.fulfilled, (state, action) => {
+      state.data = action.payload
+      state.loading = false
+      state.success = true
+      state.error = undefined
+    })
+    builder.addCase(loadCurrentUser.rejected, (state, action) => {
+      state.data = currentUserInitialState
+      state.loading = false
+      state.success = false
+      state.error = action.error.message
+    })
+  },
+})
+
+export { loadCurrentUser }
+
+export default currentUserSlice.reducer
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import clientsReducer from './slices/clientsSlice'
+import currentUserReducer from './slices/currentUserSlice'
 
 const reducer = combineReducers({
   clients: clientsReducer,
+  currentUser: currentUserReducer,
 })
 
 export default configureStore({
