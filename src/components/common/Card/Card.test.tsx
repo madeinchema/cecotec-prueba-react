83262af999e746f227d58e06115a4034955f85df
madeinchema <chemavg1@gmail.com>
2021-06-03 19:26:47 +0200

    add tests for Card comp + test "renders card with 2 buttons"

---
+++ src/components/common/Card/Card.test.tsx
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react'
+import { ButtonGroupDataSource } from '../ButtonGroup/index'
+import Card from '.'
+
+describe('<ButtonGroup /> common component', () => {
+  let buttonGroupDataSource: {
+    id: string
+    children: string
+    onClick: () => void
+  }[]
+
+  beforeEach(() => {
+    buttonGroupDataSource = [
+      {
+        id: '1',
+        children: 'Button 1',
+        onClick: () => {
+          /**/
+        },
+      },
+      {
+        id: '2',
+        children: 'Button 2',
+        onClick: () => {
+          /**/
+        },
+      },
+    ]
+  })
+
+  it('renders card with 2 buttons', () => {
+    const { getByText } = render(
+      <Card buttonGroupDataSource={buttonGroupDataSource}>Card</Card>
+    )
+    expect(getByText(/button 1/i).closest('button')).toBeInTheDocument()
+    expect(getByText(/button 2/i).closest('button')).toBeInTheDocument()
+  })
+})
