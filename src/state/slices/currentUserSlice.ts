356276c62ac934482937f5d5daf157dd5a6abca4
madeinchema <chemavg1@gmail.com>
2021-06-03 09:06:26 +0200

    add logIn & logOut thunks to currentUserSlices

--- src/state/slices/currentUserSlice.ts
+++ src/state/slices/currentUserSlice.ts
@@ -35,14 +35,59 @@ const initialState: ClientsSliceState = {
 /**
  * Thunks
  */
+// const handleLoginResponse = (): void => {}
+const USER_KEY = 'user'
+const SESSION_KEY = 'session'
+
 const loadCurrentUser = createAsyncThunk(
   'currentUser/loadCurrentUser',
   async () => {
-    const USER_KEY = 'user'
-    const currentUser: CurrentUserData = JSON.parse(
-      localStorage.getItem(USER_KEY) || '{}'
-    ) as CurrentUserData
-    return currentUser
+    const currentUserFromLocalStorage = localStorage.getItem(USER_KEY)
+    if (currentUserFromLocalStorage) {
+      const currentUser: CurrentUserData = JSON.parse(
+        currentUserFromLocalStorage
+      )
+      return currentUser
+    }
+    return undefined
+  }
+)
+
+const logInCurrentUser = createAsyncThunk(
+  'currentUser/logInCurrentUser',
+  async (userFormData: { [key: string]: string }, { dispatch }) => {
+    fetch(`${process.env.REACT_APP_SERVER_URL}/login_post`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userFormData),
+    }).then(res => res.json())
+
+    fetch(`${process.env.REACT_APP_SERVER_URL}/login_get`)
+      .then(res => res.json())
+      .then(token => {
+        localStorage.setItem(SESSION_KEY, JSON.stringify(token))
+        return fetch(`${process.env.REACT_APP_SERVER_URL}/user`, {
+          headers: {
+            token,
+          },
+        })
+          .then(res => res.json())
+          .then(data => {
+            localStorage.setItem('user', JSON.stringify(data))
+            dispatch(loadCurrentUser())
+          })
+      })
+  }
+)
+
+const logOutCurrentUser = createAsyncThunk(
+  'currentUser/logOutCurrentUser',
+  async () => {
+    localStorage.removeItem(SESSION_KEY)
+    localStorage.removeItem(USER_KEY)
   }
 )
 
@@ -54,6 +99,18 @@ export const currentUserSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: builder => {
+    builder.addCase(logInCurrentUser.pending, state => {
+      state.loading = true
+      state.success = undefined
+      state.error = undefined
+    })
+    builder.addCase(logInCurrentUser.rejected, (state, action) => {
+      state.data = currentUserInitialState
+      state.loading = false
+      state.success = false
+      state.error = action.error.message
+    })
+
     builder.addCase(loadCurrentUser.pending, state => {
       state.loading = true
       state.success = undefined
@@ -71,9 +128,27 @@ export const currentUserSlice = createSlice({
       state.success = false
       state.error = action.error.message
     })
+
+    builder.addCase(logOutCurrentUser.pending, state => {
+      state.loading = true
+      state.success = undefined
+      state.error = undefined
+    })
+    builder.addCase(logOutCurrentUser.fulfilled, (state, action) => {
+      state.data = currentUserInitialState
+      state.loading = false
+      state.success = true
+      state.error = undefined
+    })
+    builder.addCase(logOutCurrentUser.rejected, (state, action) => {
+      state.data = currentUserInitialState
+      state.loading = false
+      state.success = false
+      state.error = action.error.message
+    })
   },
 })
 
-export { loadCurrentUser }
+export { logInCurrentUser, loadCurrentUser, logOutCurrentUser }
 
 export default currentUserSlice.reducer
