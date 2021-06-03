f5c191def0bb19797c10593b5a0ef35464e7cda2
madeinchema <chemavg1@gmail.com>
2021-06-03 08:00:31 +0200

    manage loading currentUser to Redux in App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './App.scss'
@@ -5,8 +7,15 @@ import Layout from './components/layout/Layout'
 import Clients from './pages/clients/Clients'
 import Login from './pages/login/Login'
 import Products from './pages/products/Products'
+import { loadCurrentUser } from './state/slices/currentUserSlice'
 
 function App(): JSX.Element {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(loadCurrentUser())
+  }, [dispatch])
+
   return (
     <div className="App">
       <Router>
